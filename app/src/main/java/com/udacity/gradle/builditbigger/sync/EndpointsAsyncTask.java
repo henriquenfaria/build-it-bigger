3c3e2d06e507855f63e3be90a198f6a5d2d02058
Henrique Faria <henriquenunesfaria@gmail.com>
2017-02-05 21:46:23 -0200

    Add external IP address support

--- app/src/main/java/com/udacity/gradle/builditbigger/sync/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/sync/EndpointsAsyncTask.java
@@ -8,6 +8,7 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.BuildConfig;
 
 import java.io.IOException;
 
@@ -33,7 +34,7 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl(BuildConfig.MY_IP_ADDRESS)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
