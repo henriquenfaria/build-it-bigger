229829b33971a2d3e42b8cb66f2c928ed6dc2b5f
Henrique Faria <henriquenunesfaria@gmail.com>
2017-02-05 21:16:06 -0200

    Remove menu

--- app/src/main/java/com/udacity/gradle/builditbigger/activity/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/activity/MainActivity.java
@@ -3,8 +3,6 @@ package com.udacity.gradle.builditbigger.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.androidjokesdisplayer.activity.JokesDisplayerActivity;
 import com.example.androidjokesdisplayer.utils.Constants;
@@ -12,38 +10,13 @@ import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.fragment.JokeFragment;
 
 
-public class MainActivity extends AppCompatActivity
-        implements JokeFragment.OnTellJokeListener {
+public class MainActivity extends AppCompatActivity implements JokeFragment.OnTellJokeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onReturnJoke(String result) {
